0cc26816ee14c683f4169a04df43ea1f0af7c2df
DavideDiPietro <davide.dipietro3@gmail.com>
2022-06-07 14:38:23 +0200

    Realizzo controller per Admin

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('guest.home');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index') -> middleware('auth') ->name('home');
+Route::middleware('auth') 
+-> prefix('admin')
+-> name('admin.')
+-> namespace('Admin')
+-> group( function() {
+    Route::get('/', 'HomeController@index') -> name('home');
+});
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+}) -> where("any", ".*");
